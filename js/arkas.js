cfd8e0df1d1828630f56cf86360be5261a5ec57f
saefulmujibaswadasd-boop <saefulmujibaswadasd@gmail.com>
2026-03-17 09:47:58 +0700

    Update arkas.js

--- js/arkas.js
+++ js/arkas.js
@@ -91,4 +91,11 @@ function displayData(file) {
   };
   reader.readAsArrayBuffer(file);
 }
+const menuLaporanBos = document.querySelector('a[href="#laporan-bos"]');
+const laporanSection = document.getElementById('laporan-bos');
 
+menuLaporanBos.addEventListener('click', (e) => {
+  e.preventDefault();
+  laporanSection.classList.remove('hidden');
+  inputArkas.classList.add('hidden'); // sembunyikan form ARKAS
+});
